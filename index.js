180d1034e7700fd779ede1d77199204f2263f7ba
martic38 <105032725+martic38@users.noreply.github.com>
2024-06-10 22:57:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const hero = document.getElementById('hero')
+const viewMore = document.getElementById('view-more')
+
+
+hero.addEventListener("click", function(){
+    document.location.href = '/hero-post.html'
+} )
+
+viewMore.addEventListener("click", function(){
+    console.log('clicked')
+    document.getElementById('hidden1').classList.toggle("hidden")
+    document.getElementById('hidden2').classList.toggle("hidden")
+    document.getElementById('hidden3').classList.toggle("hidden")
+    viewMore.classList.toggle("hidden")
+})

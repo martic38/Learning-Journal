c88b46469f2ad8e740708b43d604d91bbbf95151
martic38 <105032725+martic38@users.noreply.github.com>
2024-06-12 22:26:02 -0700

    create vite config add pages

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'aboutme.html'),
+		main: resolve(__dirname, 'hero-post.html'),
+      },
+    },
+  },
+})

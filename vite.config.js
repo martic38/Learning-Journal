1ff888f34f06dd29d2e1e70d8e944f25ac30dd5b
martic38 <105032725+martic38@users.noreply.github.com>
2024-06-12 22:15:10 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import {defineConfig} from "vite"
+import { resolve } from 'path'
 
 export default defineConfig({
-	plugins: [
-		
-	]
+	build: {
+		rollupOptions: {
+		  input: {
+			main: resolve(__dirname, 'index.html')
+		  },
+		},
+	  },
 })
